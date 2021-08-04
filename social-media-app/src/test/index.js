14933eb9f28a76bdd753fd2466b4fc9404b64c79
Bryant Pyram <bryant.pyram@gmail.com>
2021-08-04 17:18:12 -0400

    added components, styles and working on css

---
+++ social-media-app/src/test/index.js
@@ -0,0 +1,31 @@
+// import "../../App.css";
+// import Aside from "../components/aside/Aside";
+// import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+// import Newsfeed from "../components/newsfeed/Newsfeed";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div>
+        <Navbar />
+      </div>
+{/* 
+      <div>
+        <Newsfeed />
+      </div>
+
+      <div>
+        <Aside />
+      </div>
+
+      <div>
+        <Footer />
+      </div> */}
+
+    </div>
+  );
+}
+
+export default App;
